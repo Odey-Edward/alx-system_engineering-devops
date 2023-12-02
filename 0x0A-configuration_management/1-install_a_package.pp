f8426d4fd823d9bdf7c518eeac701212a172b237
Edward_Odey <edwardodey04@gmail.com>
2023-12-02 15:59:19 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,5 @@
-#Install an especific version of flask (2.1.0)
-package { 'python3-pip':
-  ensure => installed,
-}
-
+# Install an especific version of flask (2.1.0)
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
