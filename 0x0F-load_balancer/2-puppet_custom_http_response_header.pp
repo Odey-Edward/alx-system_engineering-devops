cbfd92d50f23384a4f345fc787cf21c6567b98c3
Edward_Odey <edwardodey04@gmail.com>
2023-12-05 17:09:56 -0800

    update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,36 +1,10 @@
 #!/usr/bin/env bash
 #create a custom HTTP header response, but with Puppet
 
-
-package {'nginx':
-  ensure => installed
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => "server {
-   listen 80 default_server;
-   listen [::]:80 default_server;
-
-   root /var/www/html;
-   index index.html;
-
-   location / {
-   	add_header X-Served-By \$HOSTNAME;
-  }
-   location /redirect_me {
-      return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-   }
-   error_page 404 /404.html;
-   location = /404.html{
-      internal;
-   }
-}",
-  notify => Service['nginx'],
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
